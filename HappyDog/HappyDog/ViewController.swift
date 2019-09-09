510ed2266bfa7dcf3bda60c08b0a875997a7f97a
Liu <liuchenpei@126.com>
2019-09-09 13:59:20 +0800

    add blank project

---
+++ HappyDog/HappyDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyDog
+//
+//  Created by 刘陈佩 on 2019/9/9.
+//  Copyright © 2019 qyd.cc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HappyDog/HappyDogTests/HappyDogTests.swift
@@ -0,0 +1,34 @@
+//
+//  HappyDogTests.swift
+//  HappyDogTests
+//
+//  Created by 刘陈佩 on 2019/9/9.
+//  Copyright © 2019 qyd.cc. All rights reserved.
+//
+
+import XCTest
+@testable import HappyDog
+
+class HappyDogTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
